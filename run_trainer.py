45a0778b858eb5bc9b815820be3d854e6ba12388
xingjunyang <xingjunyang@smail.nju.edu.cn>
2025-01-13 23:00:24 +0800

    feat: setup and run on colab guideline

--- run_trainer.py
+++ run_trainer.py
@@ -15,7 +15,8 @@ def main(rank, config):
 
 
 if __name__ == "__main__":
-    config = Config("./reproduce/Baseline/Baseline-miniImageNet--ravi-Conv64F-5-Reproduce.yaml").get_config_dict()
+    # config = Config("./reproduce/Baseline/Baseline-miniImageNet--ravi-Conv64F-5-Reproduce.yaml").get_config_dict()
+    config = Config("./config/test_install.yaml").get_config_dict()
 
     if config["n_gpu"] > 1:
         os.environ["CUDA_VISIBLE_DEVICES"] = config["device_ids"]
